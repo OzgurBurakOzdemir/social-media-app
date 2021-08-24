779a30afeeca5133d9008f498bdf1382add10b5a
ozgurburakozdemir <ozgurburakozdemir@gmail.com>
2021-08-25 02:01:16 +0300

    server is modified

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import nodemon from 'nodemon';
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is listening on port: ${PORT} `)))
+    .catch((err) => { console.log(err) });
+
+mongoose.set('useFindAndModify', false)
