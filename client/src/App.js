a6614c62cf5bd0b2e92c907c4237787014d200e5
ozgurburakozdemir <ozgurburakozdemir@gmail.com>
2021-08-26 01:44:17 +0300

    Post & Form comps directory changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react'
+// eslint-disable-next-line
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import avatar from './images/avatar.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 const App = () =>
 {
     return (
-        <div>
-            <h1> App Here.</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center" > Social Network</Typography>
+                <img src={avatar} alt="memories" heigth="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3} >
+                        <Grid item xs={12} sm={7} >
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4} >
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () =>
+{
+    return (
+        <div>
+            <h1>Single Post is here</h1>
+        </div>
+    )
+}
+
+export default Post
